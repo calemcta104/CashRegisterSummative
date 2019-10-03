eeecd64d25a86aea323c367134f35a8ca4841e50
calemcta104 <calemcta104@SNS-D-130S04>
2019-10-03 13:27:41 -0400

    Add project files.

---
+++ CashRegisterSummative/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace CashRegisterSummative
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.burgerLabel = new System.Windows.Forms.Label();
+            this.fryLabel = new System.Windows.Forms.Label();
+            this.drinkLabel = new System.Windows.Forms.Label();
+            this.burgerInput = new System.Windows.Forms.TextBox();
+            this.fryInput = new System.Windows.Forms.TextBox();
+            this.drinkInput = new System.Windows.Forms.TextBox();
+            this.newOrderButton = new System.Windows.Forms.Button();
+            this.receiptButton = new System.Windows.Forms.Button();
+            this.changeLabel = new System.Windows.Forms.Label();
+            this.tenderedLabel = new System.Windows.Forms.Label();
+            this.tenderedInput = new System.Windows.Forms.TextBox();
+            this.calculateTotalButton = new System.Windows.Forms.Button();
+            this.subTotalLabel = new System.Windows.Forms.Label();
+            this.taxLabel = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.calculateChangeButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // burgerLabel
+            // 
+            this.burgerLabel.AutoSize = true;
+            this.burgerLabel.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.burgerLabel.ForeColor = System.Drawing.Color.White;
+            this.burgerLabel.Location = new System.Drawing.Point(31, 78);
+            this.burgerLabel.Name = "burgerLabel";
+            this.burgerLabel.Size = new System.Drawing.Size(126, 15);
+            this.burgerLabel.TabIndex = 0;
+            this.burgerLabel.Text = "Number of Burgers";
+            // 
+            // fryLabel
+            // 
+            this.fryLabel.AutoSize = true;
+            this.fryLabel.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fryLabel.ForeColor = System.Drawing.Color.White;
+            this.fryLabel.Location = new System.Drawing.Point(31, 113);
+            this.fryLabel.Name = "fryLabel";
+            this.fryLabel.Size = new System.Drawing.Size(112, 15);
+            this.fryLabel.TabIndex = 1;
+            this.fryLabel.Text = "Number of Fries";
+            // 
+            // drinkLabel
+            // 
+            this.drinkLabel.AutoSize = true;
+            this.drinkLabel.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.drinkLabel.ForeColor = System.Drawing.Color.White;
+            this.drinkLabel.Location = new System.Drawing.Point(31, 147);
+            this.drinkLabel.Name = "drinkLabel";
+            this.drinkLabel.Size = new System.Drawing.Size(119, 15);
+            this.drinkLabel.TabIndex = 2;
+            this.drinkLabel.Text = "Number of Drinks";
+            // 
+            // burgerInput
+            // 
+            this.burgerInput.Location = new System.Drawing.Point(164, 78);
+            this.burgerInput.Name = "burgerInput";
+            this.burgerInput.Size = new System.Drawing.Size(44, 20);
+            this.burgerInput.TabIndex = 3;
+            // 
+            // fryInput
+            // 
+            this.fryInput.Location = new System.Drawing.Point(164, 113);
+            this.fryInput.Name = "fryInput";
+            this.fryInput.Size = new System.Drawing.Size(44, 20);
+            this.fryInput.TabIndex = 4;
+            // 
+            // drinkInput
+            // 
+            this.drinkInput.Location = new System.Drawing.Point(164, 147);
+            this.drinkInput.Name = "drinkInput";
+            this.drinkInput.Size = new System.Drawing.Size(44, 20);
+            this.drinkInput.TabIndex = 5;
+            // 
+            // newOrderButton
+            // 
+            this.newOrderButton.Location = new System.Drawing.Point(343, 411);
+            this.newOrderButton.Name = "newOrderButton";
+            this.newOrderButton.Size = new System.Drawing.Size(211, 27);
+            this.newOrderButton.TabIndex = 7;
+            this.newOrderButton.Text = "New Order";
+            this.newOrderButton.UseVisualStyleBackColor = true;
+            this.newOrderButton.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // receiptButton
+            // 
+            this.receiptButton.Location = new System.Drawing.Point(34, 411);
+            this.receiptButton.Name = "receiptButton";
+            this.receiptButton.Size = new System.Drawing.Size(175, 27);
+            this.receiptButton.TabIndex = 8;
+            this.receiptButton.Text = "Print Receipt";
+            this.receiptButton.UseVisualStyleBackColor = true;
+            // 
+            // changeLabel
+            // 
+            this.changeLabel.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changeLabel.ForeColor = System.Drawing.Color.White;
+            this.changeLabel.Location = new System.Drawing.Point(33, 387);
+            this.changeLabel.Name = "changeLabel";
+            this.changeLabel.Size = new System.Drawing.Size(189, 21);
+            this.changeLabel.TabIndex = 9;
+            this.changeLabel.Text = "Change";
+            this.changeLabel.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // tenderedLabel
+            // 
+            this.tenderedLabel.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tenderedLabel.ForeColor = System.Drawing.Color.White;
+            this.tenderedLabel.Location = new System.Drawing.Point(33, 318);
+            this.tenderedLabel.Name = "tenderedLabel";
+            this.tenderedLabel.Size = new System.Drawing.Size(90, 21);
+            this.tenderedLabel.TabIndex = 10;
+            this.tenderedLabel.Text = "Tendered:";
+            // 
+            // tenderedInput
+            // 
+            this.tenderedInput.Location = new System.Drawing.Point(130, 318);
+            this.tenderedInput.Name = "tenderedInput";
+            this.tenderedInput.Size = new System.Drawing.Size(78, 20);
+            this.tenderedInput.TabIndex = 11;
+            // 
+            // calculateTotalButton
+            // 
+            this.calculateTotalButton.Location = new System.Drawing.Point(34, 182);
+            this.calculateTotalButton.Name = "calculateTotalButton";
+            this.calculateTotalButton.Size = new System.Drawing.Size(175, 27);
+            this.calculateTotalButton.TabIndex = 12;
+            this.calculateTotalButton.Text = "Calculate Totals";
+            this.calculateTotalButton.UseVisualStyleBackColor = true;
+            // 
+            // subTotalLabel
+            // 
+            this.subTotalLabel.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subTotalLabel.ForeColor = System.Drawing.Color.White;
+            this.subTotalLabel.Location = new System.Drawing.Point(31, 232);
+            this.subTotalLabel.Name = "subTotalLabel";
+            this.subTotalLabel.Size = new System.Drawing.Size(177, 21);
+            this.subTotalLabel.TabIndex = 13;
+            this.subTotalLabel.Text = "Sub Total";
+            // 
+            // taxLabel
+            // 
+            this.taxLabel.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.taxLabel.ForeColor = System.Drawing.Color.White;
+            this.taxLabel.Location = new System.Drawing.Point(31, 253);
+            this.taxLabel.Name = "taxLabel";
+            this.taxLabel.Size = new System.Drawing.Size(178, 21);
+            this.taxLabel.TabIndex = 14;
+            this.taxLabel.Text = "Tax";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalLabel.ForeColor = System.Drawing.Color.White;
+            this.totalLabel.Location = new System.Drawing.Point(31, 274);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(178, 21);
+            this.totalLabel.TabIndex = 15;
+            this.totalLabel.Text = "Total";
+            // 
+            // calculateChangeButton
+            // 
+            this.calculateChangeButton.Location = new System.Drawing.Point(33, 344);
+            this.calculateChangeButton.Name = "calculateChangeButton";
+            this.calculateChangeButton.Size = new System.Drawing.Size(175, 27);
+            this.calculateChangeButton.TabIndex = 16;
+            this.calculateChangeButton.Text = "Calculate Change";
+            this.calculateChangeButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.DarkRed;
+            this.label1.Font = new System.Drawing.Font("Millenium BdEx BT", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label1.Location = new System.Drawing.Point(-6, -4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(573, 62);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Hambuguh";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(566, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.calculateChangeButton);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.taxLabel);
+            this.Controls.Add(this.subTotalLabel);
+            this.Controls.Add(this.calculateTotalButton);
+            this.Controls.Add(this.tenderedInput);
+            this.Controls.Add(this.tenderedLabel);
+            this.Controls.Add(this.changeLabel);
+            this.Controls.Add(this.receiptButton);
+            this.Controls.Add(this.newOrderButton);
+            this.Controls.Add(this.drinkInput);
+            this.Controls.Add(this.fryInput);
+            this.Controls.Add(this.burgerInput);
+            this.Controls.Add(this.drinkLabel);
+            this.Controls.Add(this.fryLabel);
+            this.Controls.Add(this.burgerLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label burgerLabel;
+        private System.Windows.Forms.Label fryLabel;
+        private System.Windows.Forms.Label drinkLabel;
+        private System.Windows.Forms.TextBox burgerInput;
+        private System.Windows.Forms.TextBox fryInput;
+        private System.Windows.Forms.TextBox drinkInput;
+        private System.Windows.Forms.Button newOrderButton;
+        private System.Windows.Forms.Button receiptButton;
+        private System.Windows.Forms.Label changeLabel;
+        private System.Windows.Forms.Label tenderedLabel;
+        private System.Windows.Forms.TextBox tenderedInput;
+        private System.Windows.Forms.Button calculateTotalButton;
+        private System.Windows.Forms.Label subTotalLabel;
+        private System.Windows.Forms.Label taxLabel;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Button calculateChangeButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
